0b18ef633b1a451b56ea0e785c45111803d89fb1
Alice Hsieh <alicehsiehalice@gmail.com>
2019-02-10 11:49:17 -0800

    BROKEN NAR

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import screen2 from './screens/screen2';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import screen2 from "./screens/screen2";
+import calendar from "./screens/calendar";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => calendar);
--- screens/nav.js
+++ screens/nav.js
@@ -7,23 +7,26 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React from 'react';
+import React from "react";
 import HomeScreen from "./homescreen.js";
 import Screen1 from "./screen1.js";
 import Screen2 from "./screen2.js";
 import Screen3 from "./screen3.js";
 import Screen4 from "./screen4.js";
-import Screen5 from './screen5';
+import Screen5 from "./screen5";
+import Cal from "./calendar.js";
+import Slot from "./slot.js";
 
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Screen1: {screen: Screen1},
-  Screen2: {screen: Screen2},
-  Screen3: {screen: Screen3},
-  Screen4: {screen: Screen4},
-  Screen5: {screen: Screen5},
+  Home: { screen: HomeScreen },
+  Screen1: { screen: Screen1 },
+  Cal: { screen: Cal },
+  Screen3: { screen: Screen3 },
+  Screen4: { screen: Screen4 },
+  Screen5: { screen: Screen5 },
+  Slot: { screen: Slot }
 });
 
 const AppNav = createAppContainer(MainNavigator);
