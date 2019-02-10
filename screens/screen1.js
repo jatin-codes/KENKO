89153ddecf143229fbdf6f47bcb284a66379a271
John Lee <jlee14c@hotmail.com>
2019-02-09 21:57:07 -0800

    finished image carousel

--- screens/screen1.js
+++ screens/screen1.js
@@ -2,39 +2,124 @@
 
 import React, { Component } from 'react';
 import {
+  Alert,
   View,
   Text,
   StyleSheet,
-  Dimensions
+  Dimensions,
+  TouchableOpacity,
+  Button
 } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
+import {SketchCanvas} from '@terrylinla/react-native-sketch-canvas';
+
 
 const MOCK_ENTRIES = [
-  { title: 'Use your finger to mark the joints that are painful and swollen.' },
-  { title: 'Use your finger to mark the joints that are painful and swollen.' }
+  {
+    title: 'Use your finger to mark the joints that are painful and swollen.',
+    image: {
+      filename: 'hands',
+      directory: '',
+      mode: 'AspectFit'
+    },
+    index: 0
+  },
+  {
+    title: 'Use your finger to mark the joints that are painful and swollen.',
+    image: {
+      filename: 'body2',
+      directory: '',
+      mode: 'AspectFit'
+    },
+    index: 1
+  }
 ];
 
 // const sliderWidth = Dimensions.get('window').width;
-// const itemWidth = slideWidth + horizontalMargin * 2;
 const margin = 20;
+const marginBottom = 0;
 const width = Dimensions.get('window').width;
 const itemWidth = width - (margin * 2);
 const sliderWidth = width;
+
+
+const ActionBar = ({ onSave, onClear }) => (
+  <View style={actionBarStyles.container}>
+    <TouchableOpacity
+      title="Clear"
+      style={actionBarStyles.buttonClear}
+      onPress={onClear}>
+      <Text style={actionBarStyles.buttonTextClear}>Clear</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      title="Save"
+      style={actionBarStyles.buttonSave}
+      onPress={onSave}>
+      <Text style={actionBarStyles.buttonTextSave}>Save</Text>
+    </TouchableOpacity>
+  </View>
+);
 
 export default class Screen2 extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      currentIndex: 0
+    };
+    this.onClear = this.onClear.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.handleSaveImage = this.handleSaveImage.bind(this);
   }
 
   static navigationOptions = {
     header: null ,
   };
 
-  _renderItem ({item, index}) {
+  handleSaveImage(err, base64Img) {
+    if (err !== null) {
+      Alert.alert(
+        'Error Saving Image',
+        err,
+        [
+          {text: 'OK'},
+        ],
+        {cancelable: false},
+      );
+      return;
+    }
+
+    // TODO: save base64 to localstorage
+    console.warn("SUCCESS ", base64Img.slice(10));
+  }
+
+  onSave() {
+    // Save all images
+    // docs: https://github.com/terrylinla/react-native-sketch-canvas
+    MOCK_ENTRIES.forEach(item => {
+      this[`sketchCanvas${item.index}`].getBase64('png', false, true, false, false, this.handleSaveImage);
+    })
+  }
+
+  onClear(index) {
+    this[`sketchCanvas${index}`].clear();
+  }
+
+  _renderItem ({item}) {
     return (
       <View style={{flex: 1}}>
         <Text style={styles.title}>{ item.title }</Text>
+        <SketchCanvas
+          ref={(c) => this[`sketchCanvas${item.index}`] = c}
+          style={{ flex: 1 }}
+          strokeColor={'#f15f3764'}
+          strokeWidth={20}
+          localSourceImage={item.image}
+          onSketchSaved={(success, path) => {
+            console.log('onSketchSaved() ', success, " ", path);
+          }}
+        />
+        <Text style={styles.dragBar}>TODO: add icon here to prompt user to drag</Text>
       </View>
     );
   }
@@ -45,11 +130,15 @@ export default class Screen2 extends Component {
         <Carousel
           ref={(c) => { this._carousel = c; }}
           data={MOCK_ENTRIES}
-          renderItem={this._renderItem}
+          renderItem={this._renderItem.bind(this)}
           containerCustomStyle={carouselStyles.container}
           slideStyle={carouselStyles.slide}
           sliderWidth={sliderWidth}
           itemWidth={itemWidth}
+        />
+        <ActionBar
+          onSave={() => this.onSave(this._carousel.currentIndex)}
+          onClear={() => this.onClear(this._carousel.currentIndex)}
         />
       </View>
     );
@@ -59,9 +148,58 @@ export default class Screen2 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignSelf: 'stretch',
-    backgroundColor: '#343434'
+    backgroundColor: '#343434',
+  },
+  title: {
+    textAlign: 'center',
+    padding: 20,
+    fontSize: 20,
+  },
+  dragBar: {
+    height: 80,
+    borderTopColor: '#343434',
+    borderTopWidth: 1,
+    marginTop: margin,
+  }
+});
+
+const actionBarStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: 50,
+    marginLeft: margin,
+    marginRight: margin,
+    paddingTop: 5
+  },
+  buttonClear: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    backgroundColor:'#FFFFFF',
+    borderRadius: 5,
+    marginLeft: 10
+  },
+  buttonSave: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    backgroundColor:'#45afff',
+    borderRadius: 5,
+    marginLeft: 10
+  },
+  buttonTextClear: {
+    color: '#343434',
+    fontSize: 20
+  },
+  buttonTextSave: {
+    color: '#ffffff',
+    fontSize: 20
   }
 });
 
@@ -69,12 +207,12 @@ const carouselStyles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: margin,
-    marginBottom: margin
+    marginBottom: marginBottom,
   },
   slide: {
     backgroundColor: '#FFFFFF',
     borderColor: '#5c5c5c',
-    borderWidth: 2,
-
+    borderWidth: 1,
+    borderRadius: 5
   }
 });
