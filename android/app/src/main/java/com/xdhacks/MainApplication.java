a2605d58e1870ea660006b5a3680486356c93904
John Lee <jlee14c@hotmail.com>
2019-02-09 22:09:50 -0800

    updates

--- android/app/src/main/java/com/xdhacks/MainApplication.java
+++ android/app/src/main/java/com/xdhacks/MainApplication.java
@@ -3,6 +3,7 @@ package com.xdhacks;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.terrylinla.rnsketchcanvas.SketchCanvasPackage;
 import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 import com.hopding.pdflib.PDFLibPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SketchCanvasPackage(),
             new RNImgToBase64Package(),
             new RNHTMLtoPDFPackage(),
             new PDFLibPackage(),
